b5a77ef2844f63e3356bf4a3668de29afe1ffa06
Carsten Bauer <crstnbr@gmail.com>
2021-09-16 10:36:07 +0200

    alternative gpusupport function

--- src/misc.jl
+++ src/misc.jl
@@ -55,3 +55,32 @@ function gpusupport()
         return !isnothing(dlsym(handle, :likwid_gpuMarkerInit; throw_error=false))
     end
 end
+
+"""
+Alternative to `LIKWID.gpusupport()` based on analyzing the output of `likwid-topology`.
+"""
+function gpusupport_alternative()
+    x = _execute(`likwid-topology`)
+    # if there is a gpu section in the output, return true
+    contains(lowercase(x[1]), "gpu") && return true
+    # if there is a cuda related error, return true
+    contains(lowercase(x[2]), "cuda") && return true
+    # else
+    return false
+end
+
+"Run a Cmd object, returning the stdout & stderr contents plus the exit code"
+function _execute(cmd::Cmd)
+  out = Pipe()
+  err = Pipe()
+
+  process = run(pipeline(ignorestatus(cmd), stdout=out, stderr=err))
+  close(out.in)
+  close(err.in)
+
+  (
+    stdout = String(read(out)), 
+    stderr = String(read(err)),  
+    code = process.exitcode
+  )
+end
