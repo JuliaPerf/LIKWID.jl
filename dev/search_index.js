a8a936a7d46c6e041d7efec60090e93d3f504ee4
Documenter.jl <documenter@juliadocs.github.io>
2021-09-20 11:54:07 +0000

    build based on e0af409

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"marker/#Marker-API","page":"Introduction","title":"Marker API","text":"","category":"section"},{"location":"#Welcome","page":"Introduction","title":"Welcome","text":"","category":"section"},{"location":"","page":"Introduction","title":"Introduction","text":"test","category":"page"}]
+}
