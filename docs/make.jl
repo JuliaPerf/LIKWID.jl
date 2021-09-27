76e16f51a1fe31344066534bdeed63c0120f92c6
Carsten Bauer <crstnbr@gmail.com>
2021-09-27 22:21:03 +0200

    doctests

--- docs/make.jl
+++ docs/make.jl
@@ -1,13 +1,15 @@
 # push!(LOAD_PATH,"../src/")
 using Documenter, LIKWID
 
+const ci = get(ENV, "CI", "") == "true"
+
 DocMeta.setdocmeta!(LIKWID, :DocTestSetup, :(using LIKWID); recursive=true)
 makedocs(
     sitename = "LIKWID.jl",
     authors = "Carsten Bauer",
     modules = [LIKWID],
-    doctest = :fix,
-    # doctest = true,
+    # doctest = :fix,
+    doctest = true,
     pages = [
         "LIKWID" => "index.md",
         "Marker API" => [
@@ -25,9 +27,11 @@ makedocs(
     # assets = ["assets/custom.css", "assets/custom.js"]
 )
 
-deploydocs(
-    repo = "github.com/JuliaPerf/LIKWID.jl.git",
-    devbranch = "main",
-    push_preview = false,
-    # target = "site",
-)
+if ci
+    deploydocs(
+        repo = "github.com/JuliaPerf/LIKWID.jl.git",
+        devbranch = "main",
+        push_preview = false,
+        # target = "site",
+    )
+end
