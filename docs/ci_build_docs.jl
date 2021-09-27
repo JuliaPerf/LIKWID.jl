5ae97835dbcb6ad57d733a84001cf8ca1cc5c5dc
Carsten Bauer <crstnbr@gmail.com>
2021-09-27 17:46:47 +0200

    fix build docs on FAU runners

--- docs/ci_build_docs.jl
+++ docs/ci_build_docs.jl
@@ -1,8 +1,8 @@
 println("--- :julia: Instantiating project")
 using Pkg
 Pkg.instantiate()
-Pkg.activate("docs")
-Pkg.instantiate()
-push!(LOAD_PATH, @__DIR__)
+# Pkg.activate("docs")
+# Pkg.instantiate()
+# push!(LOAD_PATH, @__DIR__)
 println("+++ :julia: Building documentation")
 include("docs/make.jl")'
