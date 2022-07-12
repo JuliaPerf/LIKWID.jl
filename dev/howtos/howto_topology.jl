e35a5e2cd0cddd472b7793e9421856f18e95ab72
Documenter.jl <documenter@juliadocs.github.io>
2022-07-12 10:26:20 +0200

    build based on 473769c

--- dev/howtos/howto_topology.jl
+++ dev/howtos/howto_topology.jl
@@ -15,16 +15,17 @@ cpuinfo = LIKWID.get_cpu_info()
 
 # Query information about NUMA nodes:
 numa = LIKWID.get_numa_topology()
-numa.nodes
+
+#
 numa_node = first(numa.nodes)
 
 # ## Graphical output
 # Currently, LIKWID.jl doesn't feature a native graphical visualization of the CPU topology. However, it provides a small "wrapper function" around `likwid-topology -g` which should give you an output like this:
 LIKWID.print_cpu_topology()
 
-#src ## GPU
-#src 
-#src Query GPU topology information:
-#src topo = LIKWID.get_gpu_topology()
-#src topo.devices
-#src gpu = first(topo.devices)
+# ## GPU
+# Query GPU topology information:
+topo = LIKWID.get_gpu_topology()
+
+#
+gpu = first(topo.devices)
