50de8ad1832143373053ad7a7c23cf5eeedeec45
Documenter.jl <documenter@juliadocs.github.io>
2022-12-04 13:32:45 +0100

    build based on 05a46e6

---
+++ previews/PR47/howtos/howto_perfmon.sh
@@ -0,0 +1,1 @@
+julia --project=../.. -t 3 -E 'push!(LOAD_PATH, "../../.."); using Literate; Literate.markdown("howto_perfmon.jl", "."; execute=true, repo_root_url="https://github.com/JuliaPerf/LIKWID.jl/blob/main/docs/src/howtos")'
